ddf3867a8989d7c6203515f01a14feb5698f4025
Alvaro Favela <alvaro.favela@bisoft.com.mx>
2026-08-05 17:06:32 -0700

    Capa Infraestructure

---
+++ BiSoft.CarteleraPeliculas.Infrastructure/Mapping/PeliculaPostgreSqlConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using CarteleraPeliculas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarteleraPeliculas.Infrastructure.Mapping.PostgreSql;
+
+public class PeliculaPostgreSqlConfiguration
+    : IEntityTypeConfiguration<Pelicula>
+{
+    public void Configure(EntityTypeBuilder<Pelicula> builder)
+    {
+        builder.ToTable("peliculas");
+
+        builder.HasKey(pelicula => pelicula.Id);
+
+        builder.Property(pelicula => pelicula.Id)
+            .HasColumnName("pelicula_id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(pelicula => pelicula.Titulo)
+            .HasColumnName("titulo")
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(pelicula => pelicula.AnioEstreno)
+            .HasColumnName("anio_estreno")
+            .IsRequired();
+
+        builder.Property(pelicula => pelicula.Genero)
+            .HasColumnName("genero")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(pelicula => pelicula.PosterUrl)
+            .HasColumnName("poster_url")
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder.Property(pelicula => pelicula.Calificacion)
+            .HasColumnName("calificacion")
+            .HasPrecision(3, 1)
+            .IsRequired();
+
+        builder.Property(pelicula => pelicula.Sinopsis)
+            .HasColumnName("sinopsis")
+            .HasMaxLength(2000)
+            .IsRequired();
+
+        builder.HasIndex(pelicula => pelicula.Titulo)
+            .HasDatabaseName("ix_peliculas_titulo");
+
+        builder.ToTable(tableBuilder =>
+        {
+            tableBuilder.HasCheckConstraint(
+                "ck_peliculas_anio_estreno",
+                "anio_estreno >= 1888 AND anio_estreno <= 2100");
+
+            tableBuilder.HasCheckConstraint(
+                "ck_peliculas_calificacion",
+                "calificacion >= 0 AND calificacion <= 10");
+        });
+    }
+}
